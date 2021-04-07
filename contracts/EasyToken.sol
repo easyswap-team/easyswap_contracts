7801c8c8017469a1d82c81ce78541a3b0de79f6b
easyswap-team <team@easyswap.finance>
2021-04-07 15:48:56 +0300

    Add a mint token on the minter

--- contracts/EasyToken.sol
+++ contracts/EasyToken.sol
@@ -8,8 +8,20 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EasyToken is ERC20, Ownable {
     constructor() public ERC20("Easy Token", "ESM") {}
-
-    function mint(address _to, uint256 _amount) public onlyOwner {
+    mapping (address => bool) minters;
+    function mint(address _to, uint256 _amount) onlyMinter public  {
         _mint(_to, _amount);
+    }
+
+    function addMinter(address _address) onlyOwner public {
+        minters[_address] = true;
+    }
+
+     function delMinter(address _address) onlyOwner public {
+        minters[_address] = false;
+    }
+    modifier onlyMinter() {
+        require(minters[msg.sender],"Ownable: caller is not the owner");
+        _;
     }
 }
